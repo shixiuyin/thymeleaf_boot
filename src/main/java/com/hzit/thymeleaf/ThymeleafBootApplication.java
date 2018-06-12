67d547727feffb3626ce22838ae2e3c074b2c92a
shixiuyin <641328110@qq.com>
2018-06-12 14:18:13 +0800

    第一次提交thymeleaf

---
+++ src/main/java/com/hzit/thymeleaf/ThymeleafBootApplication.java
@@ -0,0 +1,12 @@
+package com.hzit.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBootApplication.class, args);
+	}
+}
